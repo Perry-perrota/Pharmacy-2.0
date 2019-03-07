fefb07a56dce78a21c985c5a524708fce2668a54
santa <santa@northpole.com>
2019-03-07 19:55:39 +0300

    add function to the send button

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,10 @@
 
+$(document).ready(function() {
+
+  $("#button").click(function() {
+    var name = $("input#yname").val();
+    var email = $("input#yemail").val();
+    var text = $("input#inmessage").val();
+    alert("Thanks "+name+" for reaching us");
+  });
+});
